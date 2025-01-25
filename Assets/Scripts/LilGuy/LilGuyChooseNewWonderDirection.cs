fec98d24bbf2ea51c81493d38a0effb3e3e00998
James Bland <98528984+JamesB0010@users.noreply.github.com>
2025-01-25 03:24:22 +0000

    about to grab morgans changes

--- Assets/Scripts/LilGuy/LilGuyChooseNewWonderDirection.cs
+++ Assets/Scripts/LilGuy/LilGuyChooseNewWonderDirection.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class LilGuyChooseNewWonderPos : MonoBehaviour
+public class LilGuyChooseNewWonderDirection : MonoBehaviour
 {
+    private LilGuyWonder lilGuyWonder;
+
+    private void Start()
+    {
+        this.lilGuyWonder = GetComponent<LilGuyWonder>();
+    }
+
     public void EnterState(State state)
     {
         Debug.Log("Choose a new wonder state");
+        this.lilGuyWonder.RandomizeTargetDirection();
         
         state.Transition();
     }
--- Assets/Scripts/LilGuy/LilGuyWonder.cs
+++ Assets/Scripts/LilGuy/LilGuyWonder.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class LilGuyWonder : MonoBehaviour
 {
     [SerializeField] private float timeBetweenNewWonderPos;
 
+    [SerializeField] private float speed;
+
+    private Vector3 targetDirection;
+
     private float countdownToNewWonderPos;
+
 
     private void Start()
     {
@@ -23,11 +30,23 @@ public class LilGuyWonder : MonoBehaviour
             return;
         }
 
+        ApplySteering();
         this.countdownToNewWonderPos -= Time.deltaTime;
+    }
+
+    private void ApplySteering()
+    {
+        Vector3 velocity = this.targetDirection * this.speed * Time.deltaTime;
+        transform.Translate(velocity);
     }
 
     public void ExitState(State state)
     {
         this.countdownToNewWonderPos = timeBetweenNewWonderPos;
+    }
+
+    public void RandomizeTargetDirection()
+    {
+        this.targetDirection = new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1)).normalized;
     }
 }
