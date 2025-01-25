cc0592c7772800f2c20b9a2c0c61ee24b18c0679
Will <145786907+Blujuse@users.noreply.github.com>
2025-01-25 21:26:44 +0000

    Working Collection & List

--- Assets/WillTesting/QuestManager.cs
+++ Assets/WillTesting/QuestManager.cs
@@ -2,26 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-[System.Serializable]
-public class Creature
-{
-    public string name;
-    public Sprite creatureImage;
-    public GameObject creaturePrefab;
-}
-
 public class QuestManager : MonoBehaviour
 {
     [Header("Creatures")]
-    public List<Creature> creaturesList = new List<Creature>();
-    public int numberOfCreatures = 3;
-    private GameObject[] creatures;
+    public List<EnemyTrigger> creaturePrefabs = new List<EnemyTrigger>();
+    public int spawnCount = 3;
+    private GameObject[] activeCreatures;
     private BoxCollider spawnArea;
     private Dictionary<GameObject, int> creatureDictionary;
 
@@ -33,12 +26,13 @@ public class QuestManager : MonoBehaviour
     [Header("UI")]
     public Transform paperContent;
     public GameObject creatureEntryPrefab;
+    [SerializeField] GameObject[] uiEntries = new GameObject[3];
 
     // Start is called before the first frame update
     void Start()
     {
         // Setting Creatures needed to be collected to number of spawned ones
-        requiredCreatures = numberOfCreatures;
+        requiredCreatures = spawnCount;
 
         // Get the box collider on gameobject
         spawnArea = GetComponent<BoxCollider>();
@@ -50,15 +44,12 @@ public class QuestManager : MonoBehaviour
         CreatureSpawn();
 
         // Create the list with initial creatures
-        UpdateCreatureListUI();
+        InitialiseCreatureListUI();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        // Running the Quest
-        CurrentQuest();
-        
+    {   
         //foreach (var key in creatureDictionary.Keys)
         //{
         //    foreach (var value in creatureDictionary.Values)
@@ -81,21 +72,19 @@ public class QuestManager : MonoBehaviour
     public void CreatureCollected()
     {
         currentCreatures++;
-
-        // Only update on collection
-        UpdateCreatureListUI();
+        CurrentQuest();
     }
 
     private void CreatureSpawn()
     {
-        creatures = new GameObject[numberOfCreatures];
+        activeCreatures = new GameObject[spawnCount];
 
-        for (int i = 0; i < numberOfCreatures; i++)
+        for (int i = 0; i < spawnCount; i++)
         {
             // Go through creatures that should be in the level and randomly adding them
-            int creatureToAdd = Random.Range(0, creaturesList.Count);
-            Creature currentCreature = creaturesList[creatureToAdd];
-            GameObject creaturePrefab = currentCreature.creaturePrefab;
+            int prefabIndex = Random.Range(0, creaturePrefabs.Count);
+            EnemyTrigger currentCreature = creaturePrefabs[prefabIndex];
+            GameObject creaturePrefab = currentCreature.gameObject;
 
             // Spawn creatures in area of box collider, this size is set in the inspector
             var spawnBounds = spawnArea.bounds;
@@ -104,10 +93,7 @@ public class QuestManager : MonoBehaviour
             Vector3 spawnPos = new Vector3(areaX, transform.position.y, areaZ);
 
             // Add creature to scene
-            GameObject newCreature = Instantiate(currentCreature.creaturePrefab, spawnPos, Quaternion.identity);
-
-            // Add creature to creatures array
-            creatures[i] = newCreature;
+            GameObject newCreature = Instantiate(creaturePrefab, spawnPos, Quaternion.identity);
 
             // Add creature to dictionary for tracking
             if (creatureDictionary.ContainsKey(creaturePrefab))
@@ -127,58 +113,45 @@ public class QuestManager : MonoBehaviour
         return creatureDictionary[creaturePrefabCheck];
     }
 
-    private void UpdateCreatureListUI()
+    private void DeductCreatureCount(GameObject creaturePrefabCheck)
     {
-        // Remove previous entries
-        foreach (Transform child in paperContent)
-        {
-            Destroy(child.gameObject);
-        }
+        creatureDictionary[creaturePrefabCheck]--;
+    }
 
-        // Add each creature to the ui list
-        foreach (var creature in creaturesList)
-        {
-            // Only add to list if the creature exists in the dictionary
-            if (creatureDictionary.ContainsKey(creature.creaturePrefab))
+    private void InitialiseCreatureListUI()
+    {
+        for (int i = 0; i < creaturePrefabs.Count; i++){
+            if (creatureDictionary.ContainsKey(creaturePrefabs[i].gameObject))
             {
-                // Create entry from prefab and add to display
-                GameObject creatureEntry = Instantiate(creatureEntryPrefab, paperContent);
+                uiEntries[i] = Instantiate(creatureEntryPrefab, paperContent);
 
-                // Set the image and name of the creature
-                TMP_Text creatureNameText = creatureEntry.transform.Find("Name").GetComponent<TMP_Text>();
-                Image creatureImageBox = creatureEntry.transform.Find("CreatureImg").GetComponent<Image>();
-                TMP_Text creatureCounter = creatureEntry.transform.Find("CaptureCount").GetComponent<TMP_Text>();
+                TMP_Text creatureNameText = uiEntries[i].transform.Find("Name").GetComponent<TMP_Text>();
+                creatureNameText.text = creaturePrefabs[i].data.name;
 
-                // If the creature name is found then change it
-                if (creatureNameText != null)
-                {
-                    creatureNameText.text = creature.name;
-                }
-                else
-                {
-                    Debug.Log("Can't find name");
-                }
+                Image creatureImageBox = uiEntries[i].transform.Find("CreatureImg").GetComponent<Image>();
+                creatureImageBox.sprite = creaturePrefabs[i].data.image;
 
-                // If the creature image is found then change it
-                if (creatureImageBox != null)
-                {
-                    creatureImageBox.sprite = creature.creatureImage;
-                }
-                else
-                {
-                    Debug.Log("Can't find image");
-                }
-
-                // If the creature count is found then change it
-                if (creatureCounter != null)
-                {
-                    creatureCounter.text = GetCreatureCount(creature.creaturePrefab).ToString();
-                }
-                else
-                {
-                    Debug.Log("Can't find count");
-                }
+                TMP_Text creatureCounter = uiEntries[i].transform.Find("CaptureCount").GetComponent<TMP_Text>();
+                creatureCounter.text = GetCreatureCount(creaturePrefabs[i].gameObject).ToString();
             }
         }
+    }
+
+    public void UISubtractAtID(int _id)
+    {
+        // UI Update
+        for(int i = 0; i < uiEntries.Length; i++)
+        {
+            if (i != _id)
+                continue;
+
+            DeductCreatureCount(creaturePrefabs[i].gameObject);
+
+            TMP_Text creatureCounter = uiEntries[i].transform.Find("CaptureCount").GetComponent<TMP_Text>();
+            creatureCounter.text = GetCreatureCount(creaturePrefabs[i].gameObject).ToString();
+        }
+
+        // Progression Update
+        CreatureCollected();
     }
 }
