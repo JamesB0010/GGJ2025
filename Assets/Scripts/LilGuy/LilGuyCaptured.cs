05e15b12a1277234397c26504b22a4b4be419362
James Bland <98528984+JamesB0010@users.noreply.github.com>
2025-01-26 14:02:28 +0000

    fix ai

--- Assets/Scripts/LilGuy/LilGuyCaptured.cs
+++ Assets/Scripts/LilGuy/LilGuyCaptured.cs
@@ -5,25 +5,14 @@ using UnityEngine;
 
 public class LilGuyCaptured : MonoBehaviour
 {
-    [SerializeField] Transform visualTransform;
-    [SerializeField] Animator bodyAnimator;
-    [SerializeField] Collider bodyCollider;
-    [SerializeField] SoapBubble containerBubble;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float upwardSpeed;
-
-    private Vector3 rotationDirection;
+    
 
     public void Enter(State state)
     {
-        this.rotationDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized; 
-        containerBubble  = transform.parent.GetComponent<SoapBubble>();
-        bodyAnimator.enabled = false;
-        bodyCollider.enabled = false;
+        
     }
     public void Behave(State state)
     {
-        visualTransform.Rotate(this.rotationDirection, this.rotationSpeed * Time.deltaTime);
-        visualTransform.position = containerBubble.transform.position;
+        
     }
 }
